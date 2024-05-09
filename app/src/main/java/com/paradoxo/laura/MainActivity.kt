7b38ab283040599c5357d63714dd676b3f780b6b
Junior <35709152+git-jr@users.noreply.github.com>
2024-05-09 17:45:32 -0300

    feat: gemini responser and main layout

--- app/src/main/java/com/paradoxo/laura/MainActivity.kt
+++ app/src/main/java/com/paradoxo/laura/MainActivity.kt
@@ -4,25 +4,89 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.paradoxo.laura.gemini.Gemini
 import com.paradoxo.laura.ui.theme.LauraTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
+            val scope = rememberCoroutineScope()
+
+            val gemini2 = Gemini()
+            gemini2.setupModel(
+                modelName = "gemini-pro",
+                chatMode = true,
+            )
+
+            var chatHistory by remember {
+                mutableStateOf(listOf<String>())
+            }
+
             LauraTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+
+                    MainCard(
+                        modifier = Modifier.padding(innerPadding),
+                        chatHistory = chatHistory,
+                        onSendPrompt = { prompt ->
+                            chatHistory.toMutableList().apply {
+                                add(prompt)
+                            }.also {
+                                chatHistory = it
+                            }
+                            scope.launch {
+                                gemini2.sendPromptChat(
+                                    prompt = prompt,
+                                ) { text ->
+                                    chatHistory.toMutableList().apply {
+                                        add(text)
+                                    }.also {
+                                        chatHistory = it
+                                    }
+                                }
+                            }
+                        }
                     )
                 }
             }
@@ -30,18 +94,120 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun MainCard(
+    modifier: Modifier = Modifier,
+    chatHistory: List<String> = emptyList(),
+    onSendPrompt: (String) -> Unit = {},
+) {
+    val focusManager = LocalFocusManager.current
+
+    var value by remember {
+        mutableStateOf("")
+    }
+
+    Column(
         modifier = modifier
-    )
+            .background(Color(0xFF00162C))
+    ) {
+        LazyColumn(
+            Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .weight(8f)
+        ) {
+            item {
+                Image(
+                    painter = painterResource(id = R.drawable.laura_banner_transparent),
+                    contentDescription = "Chat",
+                    contentScale = ContentScale.FillWidth,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(200.dp)
+                )
+            }
+            items(chatHistory) { message ->
+                Text(
+                    text = message,
+                    color = Color.White,
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.height(4.dp))
+        Row(
+            modifier = Modifier
+                .height(56.dp)
+                .background(Color(0xFF2BFCBD)),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            TextField(
+                placeholder = {
+                    Text(
+                        text = "Digite uma mensagem...",
+                        color = Color(0xFF00162C)
+                    )
+                },
+                value = value,
+                onValueChange = {
+                    value = it
+                },
+                modifier = Modifier
+                    .weight(5f)
+                    .background(color = Color.Transparent),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = Color.Transparent,
+                    unfocusedContainerColor = Color.Transparent,
+                    disabledContainerColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    disabledIndicatorColor = Color.Transparent,
+                ),
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Send),
+                keyboardActions = KeyboardActions(
+                    onSend = {
+                        onSendPrompt(value)
+                        value = ""
+                        focusManager.clearFocus()
+                    })
+            )
+
+
+            IconButton(onClick = {
+            }) {
+                Icon(
+                    Icons.Filled.AddCircle,
+                    "Documento",
+                    tint = Color(0xFF00162C),
+                    modifier = Modifier
+                        .weight(1f)
+                )
+            }
+
+            IconButton(onClick = {
+                onSendPrompt(value)
+                value = ""
+                focusManager.clearFocus()
+            }) {
+                Icon(
+                    Icons.Filled.Send,
+                    "Enviar",
+                    tint = Color(0xFF00162C),
+                    modifier = Modifier
+                        .weight(1f)
+                )
+            }
+        }
+
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainCardPreview() {
     LauraTheme {
-        Greeting("Android")
+        MainCard()
     }
 }
---
+++ app/src/main/java/com/paradoxo/laura/gemini/Gemini.kt
@@ -0,0 +1,80 @@
+package com.paradoxo.laura.gemini
+
+import android.graphics.Bitmap
+import com.google.ai.client.generativeai.Chat
+import com.google.ai.client.generativeai.GenerativeModel
+import com.google.ai.client.generativeai.type.Content
+import com.google.ai.client.generativeai.type.content
+
+class Gemini(
+    private val apiKey: String = "",
+) {
+
+    private val defaultInstruction =
+        "Reponda sabendo que você é: uma inteligência artificial chamada 'Laura' que auxilia estudantes na Alura, o maior ecossistema de ensino de tecnologia do Brasil e que sua irmã é se chama 'Luri'"
+    private lateinit var generativeModel: GenerativeModel
+    private lateinit var chat: Chat
+
+    init {
+        setupModel()
+    }
+
+    fun setupModel(
+        modelName: String = "gemini-pro-vision",
+        chatMode: Boolean = false
+    ) {
+        generativeModel = GenerativeModel(
+            modelName = modelName,
+            apiKey = apiKey
+        )
+
+        if (chatMode) {
+            chat = generativeModel.startChat()
+        }
+    }
+
+    suspend fun sendPrompt(
+        imageList: List<Bitmap>,
+        prompt: String
+    ) {
+        val inputContent: Content = content {
+            imageList.forEach {
+                image(it)
+            }
+            text(prompt)
+            text(defaultInstruction)
+        }
+
+        val response = generativeModel.generateContent(inputContent)
+        print(response.text)
+    }
+
+    suspend fun sendPromptChat(
+        prompt: String,
+        imageList: List<Bitmap> = emptyList(),
+        onResponse: (String) -> Unit = {}
+    ) {
+        val inputContent: Content = content {
+            imageList.forEach {
+                image(it)
+            }
+            text(prompt)
+            text(defaultInstruction)
+        }
+
+        chat.sendMessage(inputContent).let { response ->
+            print(response.text)
+            response.text?.let {
+                onResponse(it)
+            }
+        }
+
+//        chat.sendMessageStream(inputContent).collect { chunk ->
+//            print(chunk.text)
+//            chunk.text?.let { text ->
+//                onResponse(text)
+//            }
+//        }
+    }
+
+}
